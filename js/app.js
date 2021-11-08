7ef40b42ad90042913b108caf36794aa1598467d
Shaun Pour <scpour@gmail.com>
2021-11-08 10:34:19 -0500

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+    let qwerty = document.getElementById("qwerty");
+    let phrase = document.getElementById("phrase");
+    let button = document.getElementById("btn_reset");
+    let overlay = document.getElementById("overlay");
+    let missed = 0;
+    let phrases = ["Let Her Rip", "Jaws of Death", "A Cold Fish", "Cut To The Chase", "Ugly Duckling", "Knuckle Down", "A Cold Day in Hell", "Wild Goose Chase", "Fool's Gold", "Shot In the Dark"];
